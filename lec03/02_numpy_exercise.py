0700a83f0d6f3c3dba714ab006542d3aefd4fa65
lucaslinden1307 <83413758+lucaslinden1307@users.noreply.github.com>
2022-09-30 19:03:54 +0200

    Add files via upload

---
+++ lec03/02_numpy_exercise.py
@@ -0,0 +1,67 @@
+# Übung 2
+
+########################################################################################################################
+# Aufgabe 1
+# Importiere Numpy
+
+
+
+########################################################################################################################
+# Aufgabe 2
+# Erstelle aus der noten Liste einen Numpy Vektor
+noten = [1,2,3,4,5,6]
+
+
+
+########################################################################################################################
+# Aufgabe 3
+# Erstelle nun den gleichen Vektor direkt mit np.arrange
+
+
+
+########################################################################################################################
+# Aufgabe 4
+# Erstelle eine Einheitsmatrix (1 auf der Diagonale, sonst überall 0 ) der Dimension (6,6)
+
+
+
+########################################################################################################################
+# Aufgabe 5
+# Erstelle nun eine Matrix mit 1,2,...,6 auf der Diagonalen und sonst überall 0 indem du die Matrix Einheitsmatrix
+# mit dem Vektor Noten multiplizierst.
+
+
+
+########################################################################################################################
+# Aufgabe 6
+# Initialisiere eine Zufallszahlengenerator mit np.random.default_rng()
+
+
+
+########################################################################################################################
+# Aufgabe 7
+# Lasse dir eine 6x6-Matrix ausgeben, mit normalverteilten Werten mit Mittelwert 2.5 und Varianz 2
+
+
+
+########################################################################################################################
+# Aufgabe 8
+# Multipliziere nun die Matrix mit der Exponentialfunktion. Dann sind deine erzeugten Zufallsvariablen Log-normalvereilt
+
+
+
+########################################################################################################################
+# Aufgabe 9
+# Lasse dir die 6. Zeile und dort die 2.-3. Spalte von normalmatrix ausgeben
+
+
+
+########################################################################################################################
+# Aufgabe 10
+# Lasse dir alle Einträge von Normalmatrix ausgeben, die über dem empirischen Mittelwert aller Beobachtungen sind
+
+
+
+########################################################################################################################
+# Aufgabe 11
+# Berechne nun den Mittelwert und den Median der davor ausgewählten Elemente
