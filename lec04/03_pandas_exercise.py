725aae987013d5a530a48b4273aa2c1a93acd61c
lucaslinden1307 <83413758+lucaslinden1307@users.noreply.github.com>
2022-10-06 23:05:31 +0200

    Add files via upload

---
+++ lec04/03_pandas_exercise.py
@@ -0,0 +1,76 @@
+########################################################################################################################
+# Aufgabe 1
+# Importiere numpy als 'np'
+# Importiere pandas als 'pd'
+
+
+########################################################################################################################
+# Aufgabe 2
+# Lade aus der Datei `02_pandas-excelbsp.xlsx` das Sheet `BL_7-Tage-Inzidenz` als `pd.DataFrame` ein.
+# Verwende das Argument `index_col=0`, um die Bundesländer als Index zu setzen für die Zeilen.
+
+
+########################################################################################################################
+# Aufgabe 3
+# Lasse dir die ersten 5 Zeilen anzeigen (Tipp: head)
+
+
+########################################################################################################################
+# Aufgabe 4
+# Lasse dir die letzen 5 Zeilen anzeigen (Tipp: tail)
+
+
+########################################################################################################################
+# Aufgabe 5
+# Lasse dir alle Indizenzen für den 13. Oktober (2020-10-13) ausgeben
+# ...
+
+
+########################################################################################################################
+# Aufgabe 6
+# Lasse dir das Bundesland ausgeben, in dem die höchste Inzidenz geherrscht hat am 13. Oktober
+# Tipp: idxmax()
+
+
+########################################################################################################################
+# Aufgabe 7
+# Lasse dir für Baden-Württemberg die Zahlen von 01. Oktober bis einschließlich 15. Oktober ausgeben
+# Tipp: df.loc[...][...:...]
+
+
+########################################################################################################################
+# Aufgabe 8
+# Gruppiere nach Wochentagen (Mo-Fr), und bilde den jeweiligen Mittelwert über alle Observationen an dem Wochentag für jedes Bundesland
+# Tipp: df.columns.weekday, axis=1, mean
+# ...
+
+
+########################################################################################################################
+# Aufgabe 9
+# Verwende das Ergebnis des letzten Schritts und berechne den Mittelwert über alle Bundesländer für die jeweiligen Wochentage
+# Tipp: groupby, mean
+# ...
+
+
+########################################################################################################################
+# Aufgabe 10
+# Logarithmiere alle Werte im Datensatz und speichere ihn als df_log
+# Tipp: apply, np.log
+df_log = # ...
+
+
+########################################################################################################################
+# Aufgabe 11
+# Berechne nun den Mittelwert für jedes Bundesland über alle Beobachtungen
+# Tipp: mean, axis=1, skipna=True
+# ...
+
+
+########################################################################################################################
+# Aufgabe 12
+# Berechne nun den Median für jedes Bundesland über alle Beobachtungen
+# Tipp: median, axis=1, skipna=True
+df_logdiff.median(axis=1, skipna=True)
+
+
+
---
+++ lec04/05_matplotlib_exercise.py
@@ -0,0 +1,42 @@
+########################################################################################################################
+# Aufgabe 1
+# Führe diese Schritte aus:
+    # - Erstelle ein Figure-Objekt namens fig mit plt.figure().
+    # - Verwende add_axes, um eine Achse zur Figuren-Leinwand bei [0,0,1,1] hinzuzufuegen. Nenne diese neue Achse ax.
+    # - Plotte (x,y) auf den Achsen und waehle Beschriftung und Titel so, dass sie mit dem untenstehenden Plot übereinstimmen:
+
+
+########################################################################################################################
+# Aufgabe 2
+# 2a)
+# Erstelle ein Figurenobjekt und setze zwei Achsen darauf, ax1 und ax2. ax1 bei [0,0,1,1] und ax2 bei [0.2,0.5,.2,.2].
+
+
+# 2b)
+# Plotte nun (x,y) auf beiden Achsen. Und rufe dein Figurenobjekt auf, um es zu zeigen.
+
+
+########################################################################################################################
+# Aufgabe 3
+# 3a)
+# Erstelle die folgende Darstellung, indem du zwei Achsen zu einem Figurenobjekt bei[0,0,1,1] und[0.2,0.5,.4,.4] hinzufuegst.**
+
+
+# 3b)
+# Verwende nun x,y und z Arrays, um die folgende Darstellung wiederherzustellen. Beachte die x limits und ylimits auf dem eingefuegten Plot:**
+
+
+########################################################################################################################
+# Aufgabe 4
+# 4a)
+# Verwende plt.subplots(nrows=1, ncols=2), um die folgende Darstellung zu erstellen.
+
+
+# 4b)
+# Plotte nun (x,y) und (x,z) auf den Achsen. Spiele mit der Linienbreite und dem Stil herum. Außerdem sorge dafuer,
+# dass es keine Ueberlappung gibt.
+
+
+# 4c)
+# Ueberpruefe, ob du die Groesse der Darstellung aendern kannst, indem du das Argument figsize() in plt.subplots()
+# hinzufuegst. Kopiere deinen vorherigen Code**
